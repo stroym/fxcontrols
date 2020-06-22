8a915aa073c5d41209dbdd2e60f7f97a6597f6b7
Mikuláš <mikulas.papp@outlook.cz>
2020-06-22 09:00:54 +0200

    started implementing custom cell factory

--- src/main/java/cz/stroym/fxcontrols/control/SearchableListView.java
+++ src/main/java/cz/stroym/fxcontrols/control/SearchableListView.java
@@ -1,11 +1,16 @@
 package cz.stroym.fxcontrols.control;
 
 import javafx.collections.ObservableList;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.util.Callback;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.Month;
 
 @Getter
 @Setter
@@ -15,15 +20,43 @@ public class SearchableListView<T> extends ListView<T> {
   
   public SearchableListView() {
     setupSearch();
+    registerCellFactory();
   }
   
   public SearchableListView(ObservableList<T> items) {
     super(items);
     setupSearch();
+    registerCellFactory();
+  }
+  
+  //TODO popup or hide records not matching criteria and show them from the top instead of (just) scrolling to them
+  private void registerCellFactory() {
+    this.setCellFactory(new Callback<>() {
+      @Override
+      public ListCell<T> call(ListView<T> listView) {
+        return new ListCell<>() {
+          @Override
+          protected void updateItem(T item, boolean b) {
+            super.updateItem(item, b);
+            
+            setText(b ? null : getItem() == null ? "" : getItem().toString());
+            if (item == getSelectionModel().getSelectedItem()) {
+              setStyle(
+                  "-fx-background-color: transparent;" +
+                  "-fx-text-fill: orange;" +
+                  "-fx-font-weight: bold;"
+              );
+            } else {
+              setStyle("");
+            }
+          }
+        };
+      }
+    });
   }
   
   private void setupSearch() {
-    setOnKeyTyped(event -> {
+    this.setOnKeyTyped(event -> {
       //      if (this.getSelectionModel().getSelectedItem() == null || this.getSelectionModel().getSelectedItem().) {
       handleKeys(event);
       
